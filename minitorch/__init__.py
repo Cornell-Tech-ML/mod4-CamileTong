b1809f670f77dd5a1545f4201e42cd426ed553d6
CamileTong <tzshuoo@gmail.com>
2024-12-10 20:27:17 -0500

    fix library

--- minitorch/__init__.py
+++ minitorch/__init__.py
@@ -3,6 +3,7 @@
 from .testing import MathTest, MathTestVariable  # type: ignore # noqa: F401,F403
 from .fast_ops import *  # noqa: F401,F403
 from .cuda_ops import *  # noqa: F401,F403
+from .operators import *  # noqa: F401,F403
 from .tensor_data import *  # noqa: F401,F403
 from .tensor_functions import *  # noqa: F401,F403
 from .tensor_ops import *  # noqa: F401,F403
@@ -18,3 +19,6 @@ from .datasets import *  # noqa: F401,F403
 from .testing import *  # noqa: F401,F403
 from .optim import *  # noqa: F401,F403
 from . import fast_ops, cuda_ops  # noqa: F401,F403
+from .nn import *  # noqa: F401,F403
+from .fast_conv import *  # noqa: F401,F403
+from .tensor_ops import *  # noqa: F401,F403
